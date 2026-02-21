8ecd037990d15466bbed04be3d96580fc372917c
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-02-21 17:33:41 +0530

    variable folder is added

---
+++ Variables/variables.tf
@@ -0,0 +1,72 @@
+variable "ami_id" {
+  type =  string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  
+}
+
+variable "ec2_tags" {
+  type = map
+  default = {
+    Name = "variables-demo"
+    project = "Roboshop"
+    Terraform = "true"
+    Environment = "dev"
+  }
+  
+}
+
+variable "sg_name" {
+  type = string
+  default = "allow-all-terraform"
+  
+}
+variable "sg_description" {
+  type = string
+  default = "Allow TLS inbound traffic and all outbound traffic"
+  
+}
+
+variable "sg_from_port" {
+  type = number
+  default = 0
+  
+}
+
+variable "sg_to_port" {
+  type = number
+  default = 0
+  
+}
+
+variable "sg_protocol" {
+  default = -1
+  
+}
+
+variable "sg_cidr_blocks" {
+  type = list 
+  default = ["0.0.0.0/0"]   
+}
+
+variable "sg_ipv6_cidr_blocks" {
+  type = list 
+  default = ["::/0"]
+  
+}
+
+variable "sg_tags" {
+  type = map
+  default = {
+    Name = "allow-all-terraform"
+    project ="Roboshop"
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+  
+}
