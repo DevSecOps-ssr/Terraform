9ae0bccba3dfa67eed4659d2af982bac42fd5077
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-02-23 16:44:07 +0530

    New changes are added

--- Count/r53.tf
+++ Count/r53.tf
@@ -7,3 +7,12 @@ resource "aws_route53_record" "www" {
   ttl     = 1
   records = [aws_instance.example[count.index].private_ip]
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  #interpolation
+  name    = "roboshop.${var.domain_name}" 
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.example[index(var.instances, frontend)].public_ip]
+}
