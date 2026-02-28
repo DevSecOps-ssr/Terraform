1cab12a5afbfad332df0cbf9e7477d18622e6df3
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-03-01 01:20:31 +0530

    working on terraform

---
+++ terraform-aws-instance/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.sg_id
+
+  tags = {
+    Name = "Terraform"
+    project = "roboshop"
+  }
+}
+
